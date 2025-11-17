200c82914b568a12c07524f005ac71115126bab2
takuro-ryokawa <championfnf@gmail.com>
2025-11-17 21:37:40 +0900

    Updated routing: added list and todo routes,redirected root to lists.index

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,27 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TodoListIndexController;
+use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TodoListIndexController::class, 'index']);
+Route::get('/', function (){
+    return redirect()->route('lists.index');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/lists', [TodoListIndexController::class, 'index'])->name('lists.index');
+    Route::post('/lists', [TodoListIndexController::class, 'store'])->name('lists.store');
+    Route::delete('/lists/{index}', [TodoListIndexController::class, 'destroy'])->name('lists.destroy');
+    Route::get('/lists/{index}/todos', [TodoListController::class, 'index'])->name('todos.index');
+    Route::post('/lists/{index}/todos', [TodoListController::class, 'store'])->name('todos.store');
+    Route::get('/todos/{todo}', [TodoListController::class, 'edit'])->name('todos.edit');
+    Route::patch('/todos/{todo}', [TodoListController::class, 'update'])->name('todos.update');
+    Route::post('/todos/{todo}/toggle', [TodoListController::class, 'toggle'])->name('todos.toggle');
+    Route::delete('/todos/{todo}', [TodoListController::class, 'destroy'])->name('todos.destroy');
 });
 
 require __DIR__.'/auth.php';
